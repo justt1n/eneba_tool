2becdb702033e34c6573beb7f5543bc2f00995cc
tinnt <tinnt@airtrip-intl.com>
2025-08-20 10:20:25 +0700

    init eneba tool

---
+++ main.py
@@ -0,0 +1,52 @@
+import asyncio
+import logging
+from datetime import datetime
+from time import sleep
+
+from clients.google_sheets_client import GoogleSheetsClient
+from services.sheet_service import SheetService
+from utils.config import settings
+
+
+async def run_automation():
+    try:
+        g_client = GoogleSheetsClient(settings.GOOGLE_KEY_PATH)
+        sheet_service = SheetService(client=g_client)
+        payloads_to_process = sheet_service.get_payloads_to_process()
+
+        if not payloads_to_process:
+            logging.info("No payloads to process.")
+            return
+
+        for payload in payloads_to_process:
+            try:
+                hydrated_payload = sheet_service.fetch_data_for_payload(payload)
+
+                log_data = {
+                    'note': f"{payload.model_dump_json()}",
+                    'last_update': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                }
+
+                if log_data:
+                    sheet_service.update_log_for_payload(payload, log_data)
+
+                logging.info(f"Processed row {payload.row_index}, sleeping for {settings.SLEEP_TIME}s.")
+                sleep(settings.SLEEP_TIME)
+
+            except Exception as e:
+                logging.error(f"Error in flow for row {payload.row_index}: {e}")
+                sheet_service.update_log_for_payload(payload, {'note': f"Error: {e}"})
+
+    except Exception as e:
+        logging.critical(f"Đã xảy ra lỗi nghiêm trọng, chương trình dừng lại: {e}", exc_info=True)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.getLogger("httpx").setLevel(logging.ERROR)
+    logging.getLogger("httpcore").setLevel(logging.ERROR)
+
+    while True:
+        asyncio.run(run_automation())
+        logging.info("Completed processing all payloads. Next round in 10 seconds.")
+        sleep(10)
